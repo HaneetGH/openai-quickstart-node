07698cf6a5eb8150840fd06db0772be1988f12f3
Haneet Singh Chhabra <haneet555@gmail.com>
2023-05-01 00:44:10 +0530

    test

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,8 +28,12 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+  prompt: "Correct this to standard English: "+animal,
+  temperature: 0,
+  max_tokens: 60,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
